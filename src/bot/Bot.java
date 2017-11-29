f751960b3431eda2866f6196c7b796a0544429f6
Honzo <honzo.nebro@gmail.com>
2017-11-29 18:16:03 +0100

    Agregadas más expresiones regulares

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -104,31 +104,75 @@ public class Bot extends TelegramLongPollingBot {
 		if (comando.matches("[d]\\d+")) {
 			// d6
 			// es una tirada de un solo dado
-		} else if (comando.matches("\\d+[d]\\d+")) {
-			// 2d6
-			// es una tirada de varios dados
 		} else if (comando.matches("[d]\\d+[+-]\\d+")) {
 			// d5+6 d3+21
 			// es una tirada de un dado con modificador
+		}
+
+		else if (comando.matches("\\d+[d]\\d+")) {
+			// 2d6
+			// es una tirada de varios dados
 		} else if (comando.matches("\\d+[d]\\d+[+-]\\d+")) {
 			// 3d5+6 10d3+21
 			// es una tirada de varios dados con modificador
-		} else if (comando.matches("\\b(fate|Fate|FATE)")) {
+		}
+
+		else if (comando.matches("\\d+[x][d]\\d+")) {
+			// 3xd5
+			// es una tirada multiple, de 1d5 cada una
+		} else if (comando.matches("\\d+[x][d]\\d+[+-]\\d+")) {
+			// 3xd5+10
+			// es una tirada multiple, de 1d5 cada una
+			// donde aplicamos modificador
+		}
+
+		else if (comando.matches("\\d+[x]\\d+[d]\\d+")) {
+			// 6x4d6
+			// es una tirada multiple, de 4d6 cada una
+		} else if (comando.matches("\\d+[x]\\d+[d]\\d+[+-]\\d+")) {
+			// 3x2d5+10
+			// es una tirada multiple, de 2d5 cada una
+			// donde aplicamos modificador
+		}
+
+		else if (comando.matches("\\d+[d]\\d+[hlHL]\\d+")) {
+			// 3d5h1
+			// es una tirada, de 3d5 donde guardamos dados
+		} else if (comando.matches("\\d+[d]\\d+[hlHL]\\d+[+-]\\d+")) {
+			// 3d5l1+5
+			// es una tirada, de 3d5 donde guardamos dados y
+			// aplicamos el modificador
+		}
+
+		else if (comando.matches("\\d+[x]\\d+[d]\\d+[hlHL]\\d+")) {
+			// 6x4d6h3
+			// es una tirada multiple, de 4d6 donde nos quedamos con el más alto
+		} else if (comando.matches("\\d+[x]\\d+[d]\\d+[hlHL]\\d+[+-]\\d+")) {
+			// 6x4d6l3+5
+			// es una tirada multiple, de 4d6 donde nos quedamos con el más alto
+			// y aplicamos el modificador
+		}
+
+		else if (comando.matches("\\b(fate|Fate|FATE)")) {
 			// es una tirada de Fate
 		} else if (comando.matches("\\b(fate|Fate|FATE)[+-]\\d+")) {
 			// esta es una tirada fate con modificador
-		} else if (comando.matches("\\d+[x]\\d+[d]\\d+")) {
-			// 6x4d6
-			// es una tirada multiple, de 4d6
+		}
+
+		else if (comando.matches("\\b(hitos|Hitos|HITOS)")) {
+			// esta es una tirada Hitos con modificador
+		} else if (comando.matches("\\b(hitos|Hitos|HITOS)[+-]\\d+")) {
+			// esta es una tirada Hitos con modificador
 		}
 		/*
 		 * TODO
 		 * 
-		 * 6x4d6+-5 6x4d6+-50
+		 * higher y lower por encima de la cantidad de dados o 0 dados se cogen todos y
+		 * ya 3d5l0 3d5h10
 		 * 
-		 * 6x4d6h3 6x4d6h3+-5 6x4d6h3+-10
+		 * tiradas vampiro
 		 * 
-		 * 6x4d6l3 6x4d6l3+-5 6x4d6l3+-10
+		 * dados explosivos
 		 * 
 		 */
 	}
