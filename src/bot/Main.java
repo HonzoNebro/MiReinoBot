dd70c7f508b8bb366b91e7063cc9db5f203ee992
Honzo <honzo.nebro@gmaill.com>
2019-10-12 14:03:38 +0200

    Fixes de cara a V2

--- src/bot/Main.java
+++ src/bot/Main.java
@@ -3,19 +3,23 @@ package bot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		ApiContextInitializer.init();
-		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-		Bot bot = new Bot();
-		
-		try {
-			telegramBotsApi.registerBot(bot);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        Bot bot = new Bot();
+
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, e.getMessage());
+        }
+    }
 
 }
